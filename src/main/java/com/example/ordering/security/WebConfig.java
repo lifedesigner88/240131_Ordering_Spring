348542a73c2ec2e2734cd25689d72d29c41122c1
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2024-02-06 14:14:46 +0900

    2024.02.06 5th CORS

--- src/main/java/com/example/ordering/security/WebConfig.java
+++ src/main/java/com/example/ordering/security/WebConfig.java
@@ -1,14 +1,18 @@
 package com.example.ordering.security;
 
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry){
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:8081") // vue의 URL
-//                .allowedMethods("*")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//    }
-//}
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081") // vue의 URL
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
